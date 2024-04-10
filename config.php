627a6580af958ec724aafbc516dba22321b3316e
Mélissa <melissa.soares1909@gmail.com>
2024-04-10 09:54:31 +0200

    ajout services hydratation et rounting

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
-    // lors de la mise en open source, remplacer les infos concernant la base de données.
-    
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'gestionApprenants');
-    define('DB_USER', 'gestionApprenants');
-    define('DB_PWD', 'gestionApprenants');
-  define('PREFIXE', 'app_');
-    
-    // Ne pas toucher :
-    
-    define('DB_INITIALIZED', TRUE);
+// lors de la mise en open source, remplacer les infos concernant la base de données.
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gestionApprenants');
+define('DB_USER', 'gestionApprenants');
+define('DB_PWD', 'gestionApprenants');
+define('PREFIXE', 'app_');
+
+// Ne pas toucher :
+
+define('DB_INITIALIZED', TRUE);
+define('HOME_URL', '/');
