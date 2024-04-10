439127c64d4ec9d3afac2dfb6bd331559bac8dd1
Mélissa <melissa.soares1909@gmail.com>
2024-04-10 15:39:12 +0200

    css connexion

--- src/Views/Includes/navBarreCo.php
+++ src/Views/Includes/navBarreCo.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">SIMPLON</a>
-            <button class="btn btn-outline-success" type="submit">Connexion</button>
-        </div>
-    </nav>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">SIMPLON</a>
+        <button class="btn btn-outline-success" type="submit">Connexion</button>
+    </div>
+</nav>
